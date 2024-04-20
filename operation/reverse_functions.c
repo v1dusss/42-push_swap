22710281e5b9bc192604807555f05091bea1a2a8
v1dusss <vsivanat@student.42heilbronn.de>
2024-04-20 18:02:50 +0200

    rewritten rotate and reverse functions

--- operation/reverse_functions.c
+++ operation/reverse_functions.c
@@ -6,7 +6,7 @@
 /*   By: vsivanat <vsivanat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 20:40:39 by vsivanat          #+#    #+#             */
-/*   Updated: 2024/04/18 23:39:29 by vsivanat         ###   ########.fr       */
+/*   Updated: 2024/04/20 18:00:33 by vsivanat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,33 @@
 void	rra(t_stack **stack_a)
 {
 	t_stack	*temp;
-	t_stack	*temp_2;
+	t_stack	*a;
 
-	if (!*stack_a)
+	if (!*stack_a || !(*stack_a)->next)
 		return ;
+	a = *stack_a;
 	temp = lstlast(stack_a);
-	temp_2 = temp->prev;
-	temp_2->next = NULL;
-	temp_2 = lstfist(stack_a);
-	temp_2->prev = temp;
-	temp->next = temp_2;
+	temp->prev->next = NULL;
 	temp->prev = NULL;
+	temp->next = a;
+	a->prev = temp;
+	*stack_a = temp;
 }
 
 void	rrb(t_stack **stack_b)
 {
 	t_stack	*temp;
-	t_stack	*temp_2;
+	t_stack	*b;
 
-	if (!*stack_b)
+	if (!*stack_b || !(*stack_b)->next)
 		return ;
+	b = *stack_b;
 	temp = lstlast(stack_b);
-	temp_2 = temp->prev;
-	temp_2->next = NULL;
-	temp_2 = lstfist(stack_b);
-	temp_2->prev = temp;
-	temp->next = temp_2;
+	temp->prev->next = NULL;
 	temp->prev = NULL;
+	temp->next = b;
+	b->prev = temp;
+	*stack_b = temp;
 }
 
 void	rrr(t_stack **stack_a, t_stack **stack_b)
--- operation/rotate_functions.c
+++ operation/rotate_functions.c
@@ -6,7 +6,7 @@
 /*   By: vsivanat <vsivanat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 19:43:53 by vsivanat          #+#    #+#             */
-/*   Updated: 2024/04/19 09:05:06 by vsivanat         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:57:20 by vsivanat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,35 @@
 void	ra(t_stack **stack_a)
 {
 	t_stack	*temp;
-	t_stack	*temp_2;
+	t_stack	*a;
 
-	if (!*stack_a)ed`wdefgt5y6trfdeaz
+	if (!*stack_a || !(*stack_a)->next)
 		return ;
-	temp = lstfist(stack_a);
-	temp_2 = temp->next;
-	temp_2->prev = NULL;
-	temp_2 = lstlast(stack_a);
-	temp_2->next = temp;
-	temp->prev = temp_2;
-	temp->next = NULL;
+	a = *stack_a;
+	temp = a->next;
+	temp->prev = NULL;
+	a->next = NULL;
+	(*stack_a) = temp;
+	temp = lstlast(stack_a);
+	temp->next = a;
+	a->prev = temp;
 }
 
 void	rb(t_stack **stack_b)
 {
 	t_stack	*temp;
-	t_stack	*temp_2;
+	t_stack	*b;
 
-	if (!*stack_b)
+	if (!*stack_b || !(*stack_b)->next)
 		return ;
-	temp = lstfist(stack_b);
-	temp_2 = temp->next;
-	temp_2->prev = NULL;
-	temp_2 = lstlast(stack_b);
-	temp_2->next = temp;
-	temp->prev = temp_2;
-	temp->next = NULL;
+	b = *stack_b;
+	temp = b->next;
+	temp->prev = NULL;
+	b->next = NULL;
+	(*stack_b) = temp;
+	temp = lstlast(stack_b);
+	temp->next = b;
+	b->prev = temp;
 }
 
 void	rr(t_stack **stack_a, t_stack **stack_b)
